c7555dcd336d219660d3451a9a62b80542dccfb4
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-11-07 16:29:32 -0500

    add show action to snapshots controller

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -3,4 +3,9 @@ class SnapshotsController < ApplicationController
     snapshots = Snapshot.all
     render json: snapshots.as_json
   end
+
+  def show
+    snapshot = Snapshot.find_by(id: params[:id])
+    render json: snapshot.as_json
+  end
 end
