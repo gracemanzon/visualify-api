4afef9faa7db20e900cde5d0bd07ac6a3de8dd0a
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-11-07 15:14:25 -0500

    install bcrypt gem, add create action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
     users = User.all
     render json: users.as_json
   end
+
+  def create
+    user = User.new(
+      name: params["name"],
+      email: params["email"],
+      password: params["password"],
+      password_confirmation: params["password_confirmation"],
+    )
+    if user.save
+      render json: user.as_json, status: :created
+    else
+      render json: { error: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
