c222ecdf974a36159808fe49c3c41ca6335664a9
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-11-07 19:21:35 -0500

    add show action to songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,4 +3,9 @@ class SongsController < ApplicationController
     songs = Song.all
     render json: songs.as_json
   end
+
+  def show
+    song = Song.find_by(id: params[:id])
+    render json: song.as_json
+  end
 end
