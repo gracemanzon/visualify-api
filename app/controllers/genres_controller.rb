20544b263ae168130ae109c0caa0f10ff3910e67
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-11-07 19:42:38 -0500

    add destroy action to genres controller with authentication

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,6 @@
 class GenresController < ApplicationController
+  before_action :authenticate_user
+
   def index
     genres = Genre.all
     render json: genres.as_json
@@ -19,6 +21,16 @@ class GenresController < ApplicationController
       render json: genre.as_json, status: :created
     else
       render json: { error: genre.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    genre = Genre.find_by(id: params[:id])
+    if current_user.id == genre.snapshot.user_id
+      genre.destroy
+      render json: { message: "Genre successfully removed from Snapshot!" }
+    else
+      render json: { error: genre.errors.full_messages }, status: :unauthorized
     end
   end
 end
