6353c907f6727c4d76cd74cf33c5bbf160c2d8be
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-11-07 19:37:10 -0500

    add show action to genres controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,9 @@ class GenresController < ApplicationController
     genres = Genre.all
     render json: genres.as_json
   end
+
+  def show
+    genre = Genre.find_by(id: params[:id])
+    render json: genre.as_json
+  end
 end
