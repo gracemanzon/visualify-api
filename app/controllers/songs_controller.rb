6774b942ca2d33465e6c5250acbeaaefeb79fde2
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-11-07 19:25:39 -0500

    add create action to songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,4 +8,20 @@ class SongsController < ApplicationController
     song = Song.find_by(id: params[:id])
     render json: song.as_json
   end
+
+  def create
+    song = Song.new(
+      title: params[:title],
+      artist: params[:artist],
+      album: params[:album],
+      album: params[:album_art],
+      snapshot_id: params[:snapshot_id],
+    )
+
+    if song.save
+      render json: song.as_json, status: :created
+    else
+      render json: { error: song.errors.full_messages }, status: :bad_request
+    end
+  end
 end
