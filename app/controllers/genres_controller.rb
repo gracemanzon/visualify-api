6a3ff7075a10c9fbc584f4e8dea7352073c368ac
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-11-07 19:39:18 -0500

    add create action to genres controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -8,4 +8,17 @@ class GenresController < ApplicationController
     genre = Genre.find_by(id: params[:id])
     render json: genre.as_json
   end
+
+  def create
+    genre = Genre.new(
+      title: params[:title],
+      snapshot_id: params[:snapshot_id],
+    )
+
+    if genre.save
+      render json: genre.as_json, status: :created
+    else
+      render json: { error: genre.errors.full_messages }, status: :bad_request
+    end
+  end
 end
