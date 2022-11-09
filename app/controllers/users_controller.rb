e59fd64ad410b628354ef33093e864f801929633
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-11-09 12:41:23 -0500

    add users views and show action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def index
-    users = User.all
-    render json: users.as_json
+    @users = User.all
+    render template: "users/index"
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render template: "users/show"
   end
 
   def create
